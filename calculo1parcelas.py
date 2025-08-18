ae890182ad9ddd45d85db806d7baf4c172806385
Alessandra Motta <smartminds.mktdigital@gmail.com>
2025-08-18 09:44:37 -0300

    Hoje na aula 4 criamos calculos de parcelas

---
+++ calculo1parcelas.py
@@ -0,0 +1,6 @@
+# calcular as prestações do produto
+valor = int(input("Digite o valor do produto: "))
+quantidade = int(input(" Digite a quantidade de parcelas: "))
+parcelas = valor / quantidade
+
+print("O Valor da parcela será:",parcelas)
