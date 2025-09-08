18293bed6bd34259ffe42a39ef490af2bb0ea687
Alessandra Motta <smartminds.mktdigital@gmail.com>
2025-09-08 12:43:09 -0300

    Atividade aula 11 matriz Jogo do quadrado mágico

--- atividade2_matrizes_aula11.py
+++ atividade2_matrizes_aula11.py
@@ -7,9 +7,10 @@ contador=[[4,9,2],
 quadrado= int(input("Digite o numero:"))
 for x in range(0,3):
     for y in range (0,3):
-     contador = contador + quadrado[x][y]
+     contador [x] [y]= contador[x][y] + quadrado
         
 print ("digite o numero:", quadrado)
 print ("Confira o resultado das operações")
 if contador == 15: 
-   print(contador)
+   for resultado in quadrado:
+       print(resultado)
